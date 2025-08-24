746643047dd6760a6ebfd4b1c27d70fc359b1741
ChillLover <zhora.polyakov95@mail.ru>
2025-08-24 20:19:23 +0300

    minor changes

--- ui/ui.py
+++ ui/ui.py
@@ -1,22 +1,21 @@
 import gradio as gr
 import requests
 import json
-from pdf2image import convert_from_path
-from pytesseract import image_to_string
-import httpx
+# from pdf2image import convert_from_path
+# from pytesseract import image_to_string
 
 
 def get_preds(request):
-    images = convert_from_path(request)
+    # images = convert_from_path(request)
     
-    text = ""
-    for image in images:
-       page_text = image_to_string(image, lang="rus+eng")
-       text += f"{page_text}"
+    # text = ""
+    # for image in images:
+    #    page_text = image_to_string(image, lang="rus+eng")
+    #    text += f"{page_text}"
     
-    text = text.replace("\n", "")
+    # text = text.replace("\n", "")
 
-    preds = requests.post("http://api-llm:8000/check_request", json={"request": text})
+    preds = requests.post("http://api-llm:8000/check_request", json={"request": request})
 
     if preds.status_code == 200:
         preds = json.loads(preds.text)["Answer"]
